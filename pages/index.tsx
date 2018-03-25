bd02a354a7a913b54a3f5e5cc4a8958e02c394e0
Ethan Jon <e10jc@users.noreply.github.com>
2018-03-24 20:07:57 -0700

    added basic graphql query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,47 @@
-export default () => <div>Welcome to next.js!</div>
+import {InMemoryCache} from 'apollo-cache-inmemory'
+import {ApolloClient} from 'apollo-client'
+import {HttpLink} from 'apollo-link-http'
+import gql from 'graphql-tag'
+import fetch from 'node-fetch'
+import {ApolloProvider, graphql} from 'react-apollo'
+
+const client = new ApolloClient({
+  link: new HttpLink({fetch}),
+  cache: new InMemoryCache()
+})
+
+const Layout = ({children}) => (
+  <div>
+    {children}
+  </div>
+)
+
+const Home = ({data: {theories}}) => (
+  <div>
+    <h1>Theories</h1>
+    <ul>
+      {theories && theories.map(theory => (
+        <li key={theory.id}>
+          {theory.title}
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+
+const WrappedHome = graphql(gql`
+  {
+    theories {
+      id
+      title
+    }
+  }
+`)(Home)
+
+export default () => (
+  <ApolloProvider client={client}>
+    <Layout>
+      <WrappedHome />
+    </Layout>
+  </ApolloProvider>
+)
