233e47a126bce953b84dea4cee1d687cbb1a8ff9
Ethan Jon <e10jc@users.noreply.github.com>
2018-03-26 09:22:19 -0700

    ready for production deploy

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,21 @@ const createLocalConfig = ({database}) => ({
   client: 'mysql2',
   connection: {
     database,
-    user: 'user',
-    password: 'password'
+    password: 'password',
+    user: 'user'
   }
 })
 
 module.exports = {
   development: createLocalConfig({database: 'theo'}),
-  test: createLocalConfig({database: 'theo_test'}),
-  production: createLocalConfig({database: 'theo'})
+  production: {
+    client: 'mysql2',
+    connection: {
+      database: process.env.RDS_DB_NAME,
+      password: process.env.RDS_PASSWORD,
+      port: process.env.RDS_PORT,
+      user: process.env.RDS_USERNAME
+    }
+  },
+  test: createLocalConfig({database: 'theo_test'})
 }
